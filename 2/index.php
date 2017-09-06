26217bde83d0274961344a443afe51f513b032e9
OMGVee <mateescu.victor@gmail.com>
2017-09-06 16:10:56 -0400

    meh small edits for 1-3, and added some work for 4 (but will change)

--- 2/index.php
+++ 2/index.php
@@ -15,17 +15,29 @@
                 </ul>
 </header>
 <body>
+<section class="top">
 <h1>Day 2: made this navigation menu consistent across days</h1>
 <hr>
 Created this boilerplate code that I can then include in each day's project, on top<br>
 so at least there's _some_ sort of consistency :)<br>
 <hr>
+but since I haven't done much else, I thought of displaying your computer's IP Address:<br><b><span style="color:red">
 <?php
-echo "but since I haven't done much else, I thought of displaying your computer's IP Address:<br><b>";
-
-echo $_SERVER["REMOTE_ADDR"] . "</b>";
+echo $_SERVER["REMOTE_ADDR"];
 ?>
+</b>
+</span>
 
 <hr>
 
+Here's what else a website might find out from your browser:<br>
+- the User Agent string. Basically - <i><u>what browser</u></i> you're using:<br>
+<span style="color:blue">
+<?php
+//print_r($_SERVER);
+echo $_SERVER["HTTP_USER_AGENT"]; 
+?>
+</span>
+</section>
+<br><br>
 if you don't believe me, then double check with <a href="https://icanhazip.com/">the nice folks at ICanHazIP.com</a>
