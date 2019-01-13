53b1f9ff2d4f9cba1b08f2be256400e6718f6e99
OMGVee <mateescu.victor@gmail.com>
2019-01-13 02:19:43 +0000

    some sanity on the viewer

--- 6/pasteviewer.php
+++ 6/pasteviewer.php
@@ -2,11 +2,15 @@
 include ".config.inc";
 
 $shortlink = $_REQUEST['id'];
+
 $mysqli = new mysqli('127.0.0.1', "$mysql_user", "$mysql_password", "$mysql_schema");
 if ($mysqli->connect_errno) {
         echo "Error: " . $mysqli->connect_error . "\n";
         exit;
 }
+
+/* todo: some validation/sanitization of this input is a good idea */
+$shortlink = htmlspecialchars($mysqli->real_escape_string($shortlink));
 
 $result = $mysqli->query("select * from $mysql_table where shortlink='$shortlink'");
 if ($result->num_rows > 0) {
